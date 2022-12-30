f81b2c33ebfb24fed5d1cfdd82a839777b18d8a4
nghiadx.ous <nghiadx.ous@mcredit.com.vn>
2022-12-30 17:03:29 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { InputLabel,FormControl, InputAdornment, IconButton, OutlinedInput } from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import NSheading from './components/shared/heading/heading';
+const useStyle = makeStyles({
+  input:{
+  width:'300px'
+  }
+})
 function App() {
+  const [showPassword,setShowPassword]=useState(false)  
+  const [password,setPassword]=useState('')
+ const style = useStyle()
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <NSheading></NSheading>
+      <FormControl className={style.input} variant='outlined'>
+      <InputLabel>Username</InputLabel>
+      <OutlinedInput labelWidth={80}></OutlinedInput>
+      </FormControl>
+      <FormControl className={style.input} variant='outlined'>
+        <InputLabel>Password</InputLabel>
+        <OutlinedInput type={showPassword?'text':'password'} value={password}
+        onChange={e=>setPassword(e.target.value)}
+        labelWidth={80}
+        endAdornment={
+          <InputAdornment position='end'>
+            <IconButton onClick={()=>{setShowPassword(!showPassword)}}>{showPassword?<Visibility></Visibility>:<VisibilityOff></VisibilityOff>}</IconButton>
+          </InputAdornment>
+        }
+        ></OutlinedInput>
+      </FormControl>
     </div>
   );
 }
